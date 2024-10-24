615f5f551e99acaf8880742a0adf8666b9677d56
phucbv <phucbv98@vnpt.vn>
2024-10-24 11:00:43 +0700

    Update sdk

--- app/src/main/java/com/example/javaandroidsmartca/MainActivity.java
+++ app/src/main/java/com/example/javaandroidsmartca/MainActivity.java
@@ -19,14 +19,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
-import com.vnpt.smartca.SetResultListener;
-import com.vnpt.smartca.SmartCAResultCode;
-import com.vnpt.smartca.VNPTSmartCASDK;
 import com.vnpt.smartca.ConfigSDK;
 import com.vnpt.smartca.CustomParams;
-import com.vnpt.smartca.SmartCAResult;
 import com.vnpt.smartca.SmartCAEnvironment;
 import com.vnpt.smartca.SmartCALanguage;
+import com.vnpt.smartca.SmartCAResultCode;
+import com.vnpt.smartca.VNPTSmartCASDK;
 
 import java.io.Serializable;
 import java.util.Map;
@@ -109,8 +107,6 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         mVNPTSmartCA.getWaitingTransaction(transId,
-                "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBCMkEyMEVDNzgwQzI5Nzk3QjE4RTAxQkIwMDNEQTEwNjM2RTlBMzJSUzI1NiIsInR5cCI6ImF0K2p3dCIsIng1dCI6IkN5b2c3SGdNS1hsN0dPQWJzQVBhRUdOdW1qSSJ9.eyJuYmYiOjE3MTY0NTgxMjYsImV4cCI6MTcxNjQ2MTcyNiwiaXNzIjoiVk5QVFJNX0lEUCIsImF1ZCI6WyJzaWduYXR1cmUiLCJWTlBUUk1fSURQL3Jlc291cmNlcyJdLCJjbGllbnRfaWQiOiI0MTg1LTYzNzEyNzk5NTU0NzMzMDYzMy5hcHBzLnNpZ25zZXJ2aWNlYXBpLmNvbSIsImNsaWVudF90cnVzdGVkM3JkIjoidHJ1ZSIsImNsaWVudF9uYW1lIjoiVk5QVCBTbWFydENBIEFwcCBEZW1vIiwiY2xpZW50X2FwcEZpZWxkIjoiIiwiY2xpZW50X2FwcFR5cGUiOiIiLCJzdWIiOiI3YTYxNTIwNS1hOGU4LTQ1ZjUtYmVlMi0wMTk1MjIwZTVlMDAiLCJhdXRoX3RpbWUiOjE3MTY0NTgxMjYsImlkcCI6ImxvY2FsIiwicm9sZSI6IlVzZXIiLCJGdWxsTmFtZSI6IlBI4bqgTSBWxIJOIFThu6giLCJDb21wYW55QWRtaW4iOiJDb21wYW55VXNlciIsIlVzZXJHcm91cElkIjoiIiwiVWlkIjoiMDM2MDkyMDA4NjEwIiwiUGhvbmVOdW1iZXIiOiIwODQ4MzQwMDg4IiwiQWNjb3VudFR5cGVEZXNjIjoiSU5ESVZJRFVBTCIsImxhbmd1YWdlIjoidmkiLCJTZXJ2aWNlUGFjayI6IiIsIkFkbWluTG9jYWxpdHlDb2RlIjoiIiwianRpIjoiMDZCODc2N0ZCMDJEQzFDQjQxQzFDNkVBMTAwMTU2NTUiLCJpYXQiOjE3MTY0NTgxMjYsInNjb3BlIjpbImVtYWlsIiwib3BlbmlkIiwicHJvZmlsZSIsInNpZ24iLCJvZmZsaW5lX2FjY2VzcyJdLCJhbXIiOlsiY3VzdG9tIl19.n5YqPjQ3rZ7ZDCK5I09UF3fAATEq7H2N0gDPagh1KSiAcoqXvEDiBqRp1gSCISkr0IqKGJKFOxFYywFiK1vhI73bZfWxCMf8GHyMBTqRr-mw9D0gudPYfu-7S_xDTsLZfmF7V8YgC2F_h78q1H9DRBEOFum2pVVlpqVLROnZNsE7Q6AK75awwyzF4IxaaHES7GmmhNnpLr3JC_Kk-qYn2Cj2m3iO_ZWuOPNP6Q2JcXWzY2K3U7fSG1oGs2v155KkgohXOMeD6DV3QGNBFibrSjpaHFv6Lxh308MXiEjUzCisfMkCqXHVAZgzaRJKi2A2nt_zV000Rm0NgQxTYftrzg"
-                ,
                 smartCAResult -> {
                     AlertDialog.Builder builder = new AlertDialog.Builder(this);
                     builder.setTitle("Xác thực thành công");
